8b63c05feb55de46bfc8630e263ffd0b620b24b8
Renner <renneralves29@gmail.com>
2025-03-04 12:35:10 -0300

    Atualizando componentes

--- reconExterno/reconPortas.sh
+++ reconExterno/reconPortas.sh
@@ -12,15 +12,15 @@ exibir_ajuda() {
 escanear_top_1000_portas() {
     mkdir -p "/tmp/$ip"
     echo ""
-    echo "Scanning - Top 1000 portas"
-    nmap -v -sS --open -g 53 -Pn "$ip" | grep -E "open" | grep -v "Discovered" | cut -d '/' -f 1 > "/tmp/$ip/Recon1000Portas.txt"
+    echo -e "$(inserir_Cor amarelo) [+] SCANNING - TOP 1000 PORTAS $(inserir_Cor reset)"
+    nmap -v -sS --open -g 53 -Pn "$ip" | grep -E "open" | grep -v "Discovered" | grep -v "disabled"| cut -d '/' -f 1 > "/tmp/$ip/Recon1000Portas.txt"
     exibir_resultado "/tmp/$ip/Recon1000Portas.txt"
 }
 
 # Função para realizar o escaneamento de todas as portas
 escanear_todas_as_portas() {
     echo ""
-    echo "Scanning - Todas as portas"
+    echo -e "$(inserir_Cor amarelo) [+] SCANNING - TODAS AS PORTAS $(inserir_Cor reset)"
     nmap -v -sS --open -g 53 -p- -Pn "$ip" | grep -E "open" | grep -v "Discovered" | cut -d '/' -f 1 > "/tmp/$ip/ReconTodasPortas.txt"
     exibir_resultado "/tmp/$ip/ReconTodasPortas.txt"
 }
@@ -29,7 +29,7 @@ escanear_todas_as_portas() {
 analise_detalhada() {
     local portas=$(tr '\n' ',' < "/tmp/$ip/ReconTodasPortas.txt" | sed 's/,$//')
     echo ""
-    echo "Scanning - Análise detalhada"
+    echo -e "$(inserir_Cor amarelo) [+] SCANNING - REALIZANDO ANÁLISE DETALHADA DAS PORTAS DESCOBERTAS $(inserir_Cor reset)"
     nmap -v -sVC --open -g 53 -p "$portas" -Pn "$ip" > "/tmp/$ip/ReconDetalhadoPortas.txt"
     exibir_resultado "/tmp/$ip/ReconDetalhadoPortas.txt"
 }
@@ -38,12 +38,24 @@ analise_detalhada() {
 exibir_resultado() {
     local arquivo="$1"
     echo ""
-    echo "Resultado:"
+    echo -e "$(inserir_Cor verde) [+] RESULTADO: $(inserir_Cor reset)"
+    echo ""
     cat "$arquivo"
     echo ""
-    echo "-----------"
-    echo "-----------"
+    echo "$(inserir_Cor magenta)-----------"
+    echo "----------- $(inserir_Cor reset)"
     echo ""
+}
+
+inserir_Cor() {
+    case "$1" in
+        azul) echo -e "\e[34m" ;;
+        amarelo) echo -e "\e[33m" ;;
+        verde) echo -e "\e[32m" ;;
+        vermelho) echo -e "\e[31m" ;;
+        magenta) echo -e "\e[35m" ;;
+        *) echo -e "\e[0m" ;;  # Reset se a cor for inválida
+    esac
 }
 
 # Verifica se o argumento foi passado
--- reconWeb/subdomain.sh
+++ reconWeb/subdomain.sh
@@ -10,8 +10,12 @@ exibir_Ajuda() {
 realizar_Brute_Subdominios() {
    for porta in $portas; do
       formatar_Requisicao
+      echo -e "$(inserir_Cor amarelo)[+] ENCONTRANDO PADRÃO DE RESPOSTA PARA DOMÍNIOS INEXISTENTES $(inserir_Cor reset)"
+      tamanho_Padrao_Subdominio_Inexistente=$(encontrar_Padrao_Subdominio_Inexistente $host $porta)
+      formatar_Requisicao
       echo -e "$(inserir_Cor amarelo)[+] Realizando Brute-Force de subdomínios na porta $porta $(inserir_Cor reset)"
-      ffuf -u "http://$host:$porta" -w /usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1million-110000.txt -H "Host: FUZZ.$host:$porta" -fc 302 -s
+      echo -e "$(inserir_Cor verde)Encontrado: $(inserir_Cor reset)"
+      ffuf -u "http://$host:$porta" -w /usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1million-110000.txt -H "Host: FUZZ.$host:$porta" -s -fs "$tamanho_Padrao_Subdominio_Inexistente"
       wait
    done
 }
@@ -22,6 +26,12 @@ formatar_Requisicao() {
     echo ""
 }
 
+encontrar_Padrao_Subdominio_Inexistente(){
+    cat /usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1million-5000.txt | tail -n 100 > /tmp/testeSubdominio.txt
+    ffuf -u "http://$1:$2" -w /tmp/testeSubdominio.txt -H "Host: FUZZ.$1:$2" > /tmp/subdominios_inexistentes.txt
+    tamanho_Padrao_Subdominio_Inexistente=$(cat /tmp/subdominios_inexistentes.txt | cut -d "," -f 2 | cut -d ":" -f 2 | sed 's/^[[:space:]]*//' | sort | uniq -c | sort -nr | head -n1 | cut -d " " -f 6)
+    echo "$tamanho_Padrao_Subdominio_Inexistente"
+}
 
 inserir_Cor(){
     case "$1" in
@@ -33,7 +43,6 @@ inserir_Cor(){
         *) echo -e "\e[0m" ;;
     esac
 }
-
 
 if [ "$1" == "" ]; then
     exibir_Ajuda
